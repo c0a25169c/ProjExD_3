ee76a1e3779ecaee8858e940bc9b8f6c368a734c
c0a250169c <c0a250169c@edu.teu.ac.jp>
2026-06-23 14:41:04 +0900

    喜ぶこうかとん

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -168,7 +168,9 @@ def main():
 
         if bomb is not None:
             if beam is not None:
-                if beam.rct.colliderect(bomb.rct):
+                if beam.rct.colliderect(bomb.rct): # ビームで爆弾を打ち落としたら
+                    bird.change_img(6, screen)
+                    pg.display.update()
                     beam = None
                     bomb = None
 
@@ -178,7 +180,6 @@ def main():
             beam.update(screen)
         if bomb is not None:
             bomb.update(screen)   
-        bomb.update(screen)
         pg.display.update()
         tmr += 1
         clock.tick(50)
